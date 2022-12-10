75e14d9b27c76c7524cc2fe1b8733f257f07bb75
Muhammed Yaman <76904394+muhammedyaman0@users.noreply.github.com>
2022-12-10 23:22:09 +0300

    Add files via upload

---
+++ randomsifre.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>Rastgele Şifre Oluşturucu</title>
+
+</head>
+ 
+<body>
+
+	<form action="randomsifre.php" method="post">
+		Şifre Uzunluğu Seçin <br>
+		<input type="text" name="uzunluk"><br><br>
+		<input type="submit" name="buton" value="ŞİFRE OLUŞTUR!">
+		<br> <br>
+	</form>
+
+<?php
+
+function sifreolustur($chars) 
+{
+  $data = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
+  return substr(str_shuffle($data), 0, $chars);
+}
+
+	if(isset($_POST["buton"])){
+		$uzunluk=$_POST["uzunluk"];
+		echo sifreolustur($uzunluk)."\n";
+	}
+
+?>
+
+</body>
+</html>
+ 
